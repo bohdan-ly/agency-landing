03cc648d258b120f04687c68c07a4e9d95c78991
Bohdan <bohdan@klevercompany.com>
2022-06-26 22:41:14 +0300

    completed project

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,37 @@
 // Custom scripts
-// Мобильное меню бургер
+
 function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-      if (!menu.classList.contains('active')) {
-        menu.classList.add('active')
-        burger.classList.add('active-burger')
-        body.classList.add('locked')
-      } else {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-      }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-      if (window.innerWidth > 767.98) {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-      }
-    })
-  }
-  burgerMenu()
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  const navBtn = document.querySelector('.nav__btn')
 
-
-  // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-  function fixedNav() {
-    const nav = document.querySelector('nav')
-
-    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-    const breakpoint = 1
-    if (window.scrollY >= breakpoint) {
-      nav.classList.add('fixed__nav')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      navBtn.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
     } else {
-      nav.classList.remove('fixed__nav')
+      menu.classList.remove('active')
+      navBtn.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
     }
-  }
-  window.addEventListener('scroll', fixedNav)
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+      navBtn.classList.remove('active')
+    }
+  })
+}
+burgerMenu();
+
+
+
+
+
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,30 @@
 // Custom Scripts
 import '@scss/style.scss'
 import '@js/main.js';
+import '@js/select.js';
+import '@js/tabs.js';
+import '@js/libs/swiper(lib).js';
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next__btn',
+      prevEl: '.prev__btn',
+    },
+  
+  //   // Responsive breakpoints
+  //   breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  // }
+  });
